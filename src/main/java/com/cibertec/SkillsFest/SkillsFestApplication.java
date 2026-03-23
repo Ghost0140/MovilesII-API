6afce907c0912f7a4d7d4a48189674ffacf8e341
Erick <erickavendao014@gmail.com>
2026-03-23 16:14:45 -0500

    Creación del Proyecto

---
+++ src/main/java/com/cibertec/SkillsFest/SkillsFestApplication.java
@@ -0,0 +1,13 @@
+package com.cibertec.SkillsFest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkillsFestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkillsFestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cibertec/SkillsFest/SkillsFestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cibertec.SkillsFest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkillsFestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
