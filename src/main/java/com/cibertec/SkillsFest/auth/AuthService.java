a04743188b4dec0ec4cf917d3547867de330490c
Erick <erickavendao014@gmail.com>
2026-04-18 00:14:11 -0500

    JWT Analisis Radar Mejorado

--- src/main/java/com/cibertec/SkillsFest/auth/AuthService.java
+++ src/main/java/com/cibertec/SkillsFest/auth/AuthService.java
@@ -1,6 +1,5 @@
 package com.cibertec.SkillsFest.auth;
 
-
 import com.cibertec.SkillsFest.entity.Usuario;
 import com.cibertec.SkillsFest.repository.IUsuarioRepository;
 import com.cibertec.SkillsFest.security.JwtService;
@@ -9,23 +8,28 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+
 @Service
 @RequiredArgsConstructor
 public class AuthService {
 
-	private final IUsuarioRepository usuarioRepository;
+    private final IUsuarioRepository usuarioRepository;
     private final JwtService jwtService;
     private final AuthenticationManager authenticationManager;
+    private final PasswordEncoder passwordEncoder;
 
     public AuthResponse login(LoginRequest request) {
-    	
+
         authenticationManager.authenticate(
                 new UsernamePasswordAuthenticationToken(request.email(), request.password())
         );
+
         Usuario user = usuarioRepository.findByEmail(request.email())
                 .orElseThrow(() -> new RuntimeException("Usuario no encontrado"));
-        
+
         String token = jwtService.generateToken(user);
+
         return new AuthResponse(token, user.getEmail(), user.getRoles());
     }
+
 }
